58d9e3aad94070e358936cfe5baaf2af4f1a7400
Morning <morning@mstars-lab2-07s-iMac.local>
2023-01-02 12:34:30 +0800

    day-33-React-ex

--- day33/my-react-app-module/src/App.js
+++ day33/my-react-app-module/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import logo from "./arrow.svg";
+import logo1 from "./actress.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <h1>Popular Products</h1>
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="container">
+       <h1>Popular Products</h1>
+       <div className="r1">
+          <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-landscape-1648265299.jpg?crop=0.676xw:1.00xh;0.148xw,0&resize=640:*" alt=""></img>
+          <div>
+            <p><img className='lg' src={logo}/><strong>55</strong></p>
+            <p>Haught or Naight</p>
+            <p>High-minded or absent-minded?You decide</p>
+            <br/>
+            <div>
+            <p>Submitted by:
+
+            <img className='lg' src={logo1}/>
+            </p>
+
+            </div>
+          </div>
+       </div>
     </div>
   );
 }
