55535a31dc0191a19c88b1b4f612571f1715aea5
Morning <morning@mstars-lab2-07s-iMac.local>
2023-01-04 11:48:40 +0800

    day-35

--- day-34/src/productFunc.jsx
+++ day-34/src/productFunc.jsx
@@ -1,17 +1,18 @@
 import logo from "./logo.svg";
 
-function ProductFunc(){
+function ProductFunc(props){
     return(
         <div className="container">
                 <div className="r1">
-                    <img className="rounded-5"  src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-landscape-1648265299.jpg?crop=0.676xw:1.00xh;0.148xw,0&resize=640:*" alt=""></img>
+                    <img className="rounded-5"  src={props.submitterAvatarUrl} alt=""></img>
                     <div>
-                        <p><img className='lg ' src={logo}/><strong>55</strong></p>
-                        <a href="#">Haught or Naight</a>
-                        <p>High-minded or absent-minded?You decide</p>
+                        <p><img className='lg ' src={logo}/><strong>{props.id}</strong></p>
+                        <a href="#">{props.title}</a>
+                        <p>{props.description}</p>
                         <br/>
-                    
-                        <p>Submitted by:
+                        
+                        <p>{props.url}
+                        {props.votes}
 
                         <img className='lg' src={logo}/>
                         {/* <img className='lg' src="0-circle-fill.svg"/> */}
