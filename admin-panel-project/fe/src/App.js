8d4412275a26e4bffecb3151d512ffcaabd0d47b
Peacfire1 <ganzorigenkhtsogt@gmail.com>
2023-01-31 12:06:08 +0800

    admin-panel-project start

---
+++ admin-panel-project/fe/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+import Users from "./pages/Users";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Admin Panel Project</h1>
+      <Routes>
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
