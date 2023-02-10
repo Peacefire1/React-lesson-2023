e4b90a0d6f7578b0180ffcf2b7ca34674a00dcf2
Peacfire1 <ganzorigenkhtsogt@gmail.com>
2023-02-10 12:30:19 +0800

    fail swiper

---
+++ day-60-mui/src/data/images.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1675891427182-60f29d702633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1674166111222-c25b3b5b9f3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1675026651173-4510078c6284?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1673905088926-eab6023079d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1675285972028-45dc2add07be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
+
+export default images;
