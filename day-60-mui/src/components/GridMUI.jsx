873b46ff6a4763c0e6410be80257d9befdb499ab
Peacfire1 <ganzorigenkhtsogt@gmail.com>
2023-02-10 10:59:18 +0800

    day-60

---
+++ day-60-mui/src/components/GridMUI.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Box, createTheme, Grid, ThemeProvider } from "@mui/material";
+
+const GridMUI = () => {
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 375,
+        md: 769,
+        lg: 1140,
+        xl: 1440,
+      },
+    },
+  });
+  return (
+    <div>
+      <h1>Day-60-MUI Grid Box </h1>
+      <main>
+        <ThemeProvider theme={theme}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Box
+                sx={{
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
+                }}
+              >
+                First Column
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Box
+                sx={{
+                  display: {
+                    xs: "block",
+                    sm: "block",
+                    md: "none",
+                  },
+                }}
+              >
+                Second Column
+              </Box>
+            </Grid>
+          </Grid>
+        </ThemeProvider>
+      </main>
+    </div>
+  );
+};
+
+export default GridMUI;
