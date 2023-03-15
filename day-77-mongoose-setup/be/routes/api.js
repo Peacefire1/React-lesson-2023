605df869dceab306ac5cbe77df0f93b159b300a9
Peacfire1 <ganzorigenkhtsogt@gmail.com>
2023-03-15 12:10:39 +0800

    day-77 mongoose

---
+++ day-77-mongoose-setup/be/routes/api.js
@@ -0,0 +1,31 @@
+const { request, response } = require("express");
+const express = require("express");
+const Router = express.Router();
+const User = require("../models/Users");
+
+Router.get("/users", async (request, response) => {
+  const result = await User.find({});
+  response.json({ data: result });
+});
+
+Router.post("/user", async (request, response) => {
+  const body = request.body;
+
+  const newUser = new User(body);
+  const result = await newUser.save();
+  console.log(result);
+  response.json({
+    data: result,
+  });
+});
+
+Router.get("/user", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  response.json({
+    data: result,
+  });
+});
+
+module.exports = Router;
