3f729449df78edbac83db0109cef0fc9e9ed6583
Morning <morning@mtars-lab2-17s-iMac.local>
2023-04-26 12:04:24 +0800

    day-102 next auth Oauth git hub-r holboh

---
+++ day-102-outh-with-nextauth/src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+
+export default function Layout({children}:{children:ReactNode}){
+return(
+    <div>
+        <Header/>
+        <main>
+            {children}
+        </main>
+    </div>
+)
+}
---
+++ day-102-outh-with-nextauth/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+
+export default function IndexPage(){
+  return(
+    <Layout>
+      <h1>Next.js Example</h1>
+      <p>
+        Here is my NextJS NextAuth application
+      </p>
+    </Layout>
+  )
+}
